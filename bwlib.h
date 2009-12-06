0eaa46d78feda37b7c1cb33d81e42b797c4ef1c0
xdliuchang <xdliuchang@76f41e0e-d1f7-11de-8607-c5ce5d18c61a>
2009-12-06 12:21:57 +0000

    must be some problems,but the bwlib.h is completed
    git-svn-id: http://bwproject.googlecode.com/svn/trunk/bwlib@45 76f41e0e-d1f7-11de-8607-c5ce5d18c61a

--- bwlib.h
+++ bwlib.h
@@ -30,31 +30,50 @@
 
 typedef struct bwlib_rb_node{
 	/* TODO: Add the define of rb node here */
+	unsigned long parent;
+	struct bwlib_rb_node *rb_left, *rb_right;
 }bwlib_rb_node_t;
 
 typedef struct bwlib_rb_root{
 	/* TODO: Add the define of rb root node here */
+	bwlib_rb_node_t *root_node;	
 }bwlib_rb_root_t;
 
+ 
+bwlib_rb_node_t null_node = {
+	0,NULL,NULL
+};
+
+
+extern int bwlib_rb_init(bwlib_rb_root_t *);
 extern void bwlib_rb_insert_color(bwlib_rb_node_t *, bwlib_rb_root_t *);
 extern void bwlib_rb_erase(bwlib_rb_node_t *, bwlib_rb_root_t *);
 extern void bwlib_rb_clear_node(bwlib_rb_node_t *);
 extern bwlib_rb_node_t *bwlib_rb_first(bwlib_rb_root_t *);
 extern bwlib_rb_node_t *bwlib_rb_last(bwlib_rb_root_t *);
-extern bwlib_rb_node_t *bwlib_rb_next(bwlib_rb_node_t *)
-extern bwlib_rb_node_t *bwlib_rb_prev(bwlib_rb_node_t *)
+extern bwlib_rb_node_t *bwlib_rb_next(bwlib_rb_node_t *);
+extern bwlib_rb_node_t *bwlib_rb_prev(bwlib_rb_node_t *);
 extern void bwlib_rb_link_node(bwlib_rb_node_t *, bwlib_rb_node_t *,
-			       bwlib_rb_node_t **)
+			       bwlib_rb_node_t **);
 
 /* TODO: Fix the two utility macro define here */
-#define bwlib_rb_enty(ptr, type, member)
-#define BWLIB_RB_ROOT
+#define EMPTY_ROOT -2
+#define container_of(ptr, type, member) ((type *)((char *)ptr - (char *)&((type *)0)->member))
+#define bwlib_rb_entry(ptr, type, member) container_of(ptr, type, member) 
+#define BWLIB_RB_ROOT (bwlib_rb_root_t) { NULL,} 
+
+#define rb_color(x) ((x)->parent & 3)
+#define rb_parent(x) ((bwlib_rb_node_t *)((x)->parent & (~3)))
+#define rb_set_red(x) ((x)->parent &= ~1)
+#define rb_set_black(x) ((x)->parent |= 1) 
+#define rb_is_red(x) (!rb_color(x))
+#define rb_is_black(x) (rb_color(x))
+
+#define is_null_node(x) ((x) == &null_node)
 
 /* 
  * TODO: Add the radix tree define here.
  */
-
-
 
 #endif /* _BWLIB_H */
 
