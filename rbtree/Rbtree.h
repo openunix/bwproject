574717df6430c7fd549a2a902700dbf41b67ee6b
clchnu <clchnu@76f41e0e-d1f7-11de-8607-c5ce5d18c61a>
2009-12-16 15:21:16 +0000

    git-svn-id: http://bwproject.googlecode.com/svn/trunk/bwlib@69 76f41e0e-d1f7-11de-8607-c5ce5d18c61a

---
+++ rbtree/Rbtree.h
@@ -0,0 +1,60 @@
+/* 
+ * Copyright (c) 2009 Tianjin Zhongke Blue Whale 
+ *               Information Technologies Co., Ltd
+ * 
+ * rbtree.c - implement of the functions 
+ * exported by bwlib.
+ * 
+ * Dependencies: None (Should be self-defined)
+ *
+ * ChangeLog:
+ *  2009-12-2 Initial Created
+ * ToDo:
+ *  None
+ */
+
+ /* 
+  * Copyright (c) 2009 Institute of Computing Technology,
+  *                    Chinese Academy of Sciences
+  * 
+  * Author:  
+  */
+
+#ifndef RB_NODE
+#define RB_NODE
+
+#define RB_RED 0
+#define RB_BLACK 1
+
+#define NULL    ((void *)0)
+
+/* when use static inline*/ 
+#ifdef _MSC_VER
+#define INLINE __inline
+#else
+#define INLINE inline
+#endif
+
+#include "../bwlib.h"
+
+#define rb_parent(x) ((bwlib_rb_node_t *)((x)->parent & (~3)))
+#define rb_color(x) ((x)->parent & 1)
+#define rb_is_red(x) (!rb_color(x))
+#define rb_is_black(x) (rb_color(x))
+#define rb_set_red(x) ((x)->parent &= ~1)
+#define rb_set_black(x) ((x)->parent |= 1) 
+
+static INLINE void rb_set_parent(bwlib_rb_node_t *rb, bwlib_rb_node_t *p)
+{
+	rb->parent = (rb->parent & 3) | (unsigned long)p;
+}
+static INLINE void rb_set_color(bwlib_rb_node_t *rb, int color)
+{
+	rb->parent = (rb->parent & ~1) | color;
+}
+
+#define RB_EMPTY_ROOT(root)	((root)->rb_node == NULL)
+#define RB_EMPTY_NODE(node)	(parent(node) == node)
+#define RB_CLEAR_NODE(node)	(rb_set_parent(node, node))
+
+#endif
