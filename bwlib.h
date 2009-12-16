1fc6ca3a2ff49ed523a21501cbc89179fb1089ae
clchnu <clchnu@76f41e0e-d1f7-11de-8607-c5ce5d18c61a>
2009-12-16 15:22:07 +0000

    git-svn-id: http://bwproject.googlecode.com/svn/trunk/bwlib@71 76f41e0e-d1f7-11de-8607-c5ce5d18c61a

--- bwlib.h
+++ bwlib.h
@@ -1,3 +1,17 @@
+/*
+ *----------------------------------------------------------------------------
+ *	LSI Corporation
+ *	1621 Barber Lane
+ *	Milpitas, California 95035
+ *----------------------------------------------------------------------------
+ * Copyright ?2004-2006, LSI Corporation All Rights Reserved.
+ * LSI's source code is an unpublished work and the use of copyright
+ * notice does not imply otherwise. This source code contains confidential,
+ * trade secret material of LSI Corporation. Any attempt or
+ * participation in deciphering, decoding, reverse engineering or
+ * in any way altering the source code is strictly prohibited, unless the
+ * prior written consent of LSI Corporation.
+=======
 /* 
  * Copyright (c) 2009 Tianjin Zhongke Blue Whale 
  *               Information Technologies Co., Ltd
@@ -19,10 +33,9 @@
   * 
   * Author:  
   */
-#include <stdio.h>
+
 #ifndef _BWLIB_H
 #define _BWLIB_H
-
 
 /* 
  * Attention: We wouldn't include any bwoss layer header here. Do this
@@ -37,11 +50,12 @@ typedef struct bwlib_rb_node{
 
 typedef struct bwlib_rb_root{
 	/* TODO: Add the define of rb root node here */
-	bwlib_rb_node_t *root_node;	
+	bwlib_rb_node_t *rb_node;	
 }bwlib_rb_root_t;
 
+typedef unsigned int uint32_t;
 
-extern int bwlib_rb_init(bwlib_rb_root_t *);
+extern void bwlib_rb_init(bwlib_rb_root_t *);
 extern void bwlib_rb_insert_color(bwlib_rb_node_t *, bwlib_rb_root_t *);
 extern void bwlib_rb_erase(bwlib_rb_node_t *, bwlib_rb_root_t *);
 extern void bwlib_rb_clear_node(bwlib_rb_node_t *);
@@ -53,14 +67,37 @@ extern void bwlib_rb_link_node(bwlib_rb_node_t *, bwlib_rb_node_t *,
 			       bwlib_rb_node_t **);
 
 /* TODO: Fix the two utility macro define here */
-#define EMPTY_ROOT -2
-#define container_of(ptr, type, member) ((type *)((char *)ptr - (char *)&((type *)0)->member))
-#define bwlib_rb_entry(ptr, type, member) container_of(ptr, type, member) 
-#define RB_ROOT	(struct rb_root) { NULL, }
+#define bwlib_rb_entry(ptr, type, member)  ((type *)((char *)ptr - (char *)(&((type *)0)->member)))
+#define BWLIB_RB_ROOT (struct bwlib_rb_root){NULL,}
 
-/* 
- * TODO: Add the radix tree define here.
- */
 
-#endif /* _BWLIB_H */
+#define RADIX_TREE_MAP_SHIFT	6	
+#define RADIX_TREE_MAP_SIZE	(1UL << RADIX_TREE_MAP_SHIFT) /*It represents the number of slots.*/
+#define RADIX_TREE_MAP_MASK	(RADIX_TREE_MAP_SIZE-1)  /*it is used to caculate the index of each level*/
+#define RADIX_TREE_SUCCESS 0
+#define RADIX_TREE_FAILED 1
 
+typedef struct bwlib_radix_tree_node 
+{
+	uint32_t	height;		
+	uint32_t	count;					 /*the number of slots which are not empty*/
+	void		*slots[RADIX_TREE_MAP_SIZE]; 
+}bwlib_radix_tree_node ;
+
+typedef struct bwlib_radix_tree_root 
+{
+	uint32_t		height;
+	struct bwlib_radix_tree_node	*rnode;
+}bwlib_radix_tree_root;
+
+void bwlib_radix_tree_init(bwlib_radix_tree_root*);
+extern uint32_t bwlib_radix_tree_insert(bwlib_radix_tree_root *, uint32_t, void *);
+extern void* bwlib_radix_tree_lookup(bwlib_radix_tree_root *, uint32_t);
+extern void* bwlib_radix_tree_delete(bwlib_radix_tree_root *, uint32_t);
+
+
+#pragma pack(pop)
+
+#endif /* LSI_MEGA_SWR_COMMON_DEFS_H */
+
+/* vim: set ts=4 sw=4 nowrap: */
